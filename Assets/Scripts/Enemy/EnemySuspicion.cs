9b34df64e358ecf4b272c2e54ee66a9fac9c59fc
Gretron <89054564+Gretron@users.noreply.github.com>
2023-04-25 17:27:31 -0800

    test: add to enemy suspicion system test scene

---
+++ Assets/Scripts/Enemy/EnemySuspicion.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enemy Suspicion Behaviour
+/// </summary>
+public class EnemySuspicion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
